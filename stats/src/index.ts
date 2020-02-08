c420cde94acce9a3e72db74bad8dedd9f54aa70f
a.abramyants <a.abramyants@fun-box.ru>
2020-02-08 22:54:09 +0300

    [Stats] Source file reading.

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello there');
+import fs from 'fs';
+import path from 'path';
+
+const sourceFile = 'football.csv';
+const sourceFilePath = path.resolve(__dirname, `../${sourceFile}`);
+
+const matches = fs
+  .readFileSync(sourceFilePath, {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => (
+    row.split(',')
+  ));
+
+console.log(matches);
