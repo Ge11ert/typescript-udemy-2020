4e17fffa97a84b105711d834e6d0d90789d925aa
a.abramyants <a.abramyants@fun-box.ru>
2020-02-08 23:30:58 +0300

    [Stats] Separate reader class.

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,25 @@
-import fs from 'fs';
 import path from 'path';
+import { CsvFileReader } from './CsvFileReader';
+
+enum Winner {
+  HOME = 'H',
+  AWAY = 'A',
+  DRAW = 'D',
+}
 
 const sourceFile = 'football.csv';
 const sourceFilePath = path.resolve(__dirname, `../${sourceFile}`);
+const reader = new CsvFileReader(sourceFilePath);
+reader.read();
 
-const matches = fs
-  .readFileSync(sourceFilePath, {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => (
-    row.split(',')
-  ));
+let manUnitedWins = 0;
 
-console.log(matches);
+reader.data.forEach((match: string[]) => {
+  if (match[1] === 'Man United' && match[5] === Winner.HOME) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === Winner.AWAY) {
+    manUnitedWins++;
+  }
+});
+
+console.log(`Manchester United won ${manUnitedWins} games`);
