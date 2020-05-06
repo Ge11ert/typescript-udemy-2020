4e111861caac8190f90fa2f5961baa128972448c
a.abramyants <a.abramyants@fun-box.ru>
2020-05-07 00:19:32 +0300

    [Exp] Extend request interface.

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import { router } from './routes/login-routes';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
--- express/src/routes/login-routes.ts
+++ express/src/routes/login-routes.ts
@@ -2,14 +2,28 @@ import { Request, Response, Router } from 'express';
 import loginFormTemplate from '../templates/login-form';
 import indexTemplate from '../templates/index';
 
+interface RequestWithBody extends Request {
+  body: Record<string, string|undefined>;
+}
+
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/', (req: RequestWithBody, res: Response) => {
   res.send(indexTemplate);
 });
 
-router.get('/login', (req: Request, res: Response) => {
+router.get('/login', (req: RequestWithBody, res: Response) => {
   res.send(loginFormTemplate);
-})
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email !== undefined) {
+    res.send(email.toUpperCase());
+  } else {
+    res.send('Email is required');
+  }
+});
 
 export { router };
